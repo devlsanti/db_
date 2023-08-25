d758e1be90d0d36da19bbba59c7cc7c80a7f576a
devlsanti <143213000+devlsanti@users.noreply.github.com>
2023-08-25 14:34:54 -0300

    Add files via upload

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "bdaws.cjmnwzassu0g.us-east-1.rds.amazonaws.com";
+$username = "Ldevaws";
+$password = "Leo270752#";
+$database = "bd_concessionaria";
+?>
